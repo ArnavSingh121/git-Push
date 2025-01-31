7a3e6f26f7aece0dc1d5c9f82d21a3a4fd76ceec
Arnav Singh <arnavsin218@gmail.com>
2025-01-31 12:54:45 -0800

    Commit message

---
+++ Project 0 base code.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomNumberGame {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int targetNumber = random.nextInt(100) + 1; // Generates a number between 1 and 100
+        Scanner scanner = new Scanner(System.in);
+        int guess = 0;
+
+        System.out.println("Welcome to the Random Number Guessing Game!");
+        System.out.println("Try to guess the number between 1 and 100.");
+
+        while (guess != targetNumber) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+
+            if (guess < targetNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > targetNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the correct number.");
+            }
+        }
+        scanner.close();
+    }
+}
